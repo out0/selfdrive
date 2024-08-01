ae1e787a9865817c08d7b0c3d9ca1be26840aa1e
out0 <cristianoo@gmail.com>
2024-08-01 13:16:04 -0300

    logging

---
+++ utils/logging.py
@@ -0,0 +1,19 @@
+
+
+LOGGING_LEVEL = 3
+LOGGING_FILE = "run.log"
+
+class Telemetry:
+    
+    @classmethod
+    def log (cls, level: int, module: str, message: str) -> None:
+        if level < LOGGING_LEVEL: return
+        
+        msg = f"[{module}] {message}"
+        
+        if LOGGING_FILE is None:
+            print (msg)
+        else:
+            f = open(LOGGING_FILE, "a")
+            f.write(f"{msg}\n")
+            f.close()
