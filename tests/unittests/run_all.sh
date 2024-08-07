8d79ed7160f214f99f8deee1c84baf9eb42a2b0a
out0 <cristianoo@gmail.com>
2024-08-07 07:21:36 -0300

    test integration for cudac utils

--- tests/unittests/run_all.sh
+++ tests/unittests/run_all.sh
@@ -2,4 +2,5 @@
 python3 -m unittest discover -v model/
 python3 -m unittest discover -v data/
 python3 -m unittest discover -v motion/
+python3 -m unittest discover -v utils/
 #python3 -m unittest discover -v slam/
