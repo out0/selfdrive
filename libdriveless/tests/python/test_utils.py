4fc2185446f89bef11dbe9eab9058a8820cec464
out0 <cristianoo@gmail.com>
2025-08-16 00:58:28 -0300

    testfix

--- libdriveless/tests/python/test_utils.py
+++ libdriveless/tests/python/test_utils.py
@@ -14,14 +14,12 @@ class AssertTolerance:
             
 
 def fix_cv2_import():
-    try:
-        from cv2.version import ci_build, headless
-        ci_and_not_headless = ci_build and not headless
-    except:
-        pass
+    import cv2
+    # Safe check for OpenCV build flags
+    ci_and_not_headless = getattr(cv2, "ci_build", False) and not getattr(cv2, "headless", False)
+
     if sys.platform.startswith("linux") and ci_and_not_headless:
-        if "QT_QPA_PLATFORM_PLUGIN_PATH" in os.environ:
-            os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+        os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH", None)
+
     if sys.platform.startswith("linux") and ci_and_not_headless:
-        if "QT_QPA_FONTDIR" in os.environ:
-            os.environ.pop("QT_QPA_FONTDIR")
+        os.environ.pop("QT_QPA_FONTDIR", None)
