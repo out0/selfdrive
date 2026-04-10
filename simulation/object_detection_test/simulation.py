9bc45f32ca3f615e757c06a02dd4adad572c0c42
out0 <cristianoo@gmail.com>
2026-04-10 16:51:03 -0300

    adding yolo detection

--- simulation/object_detection_test/simulation.py
+++ simulation/object_detection_test/simulation.py
@@ -1,10 +1,7 @@
 from carladriver import CarlaSimulation, CarlaEgoVehicle
 from pydriveless import EgoVehicle, DiscreteComponent, Camera
 import time, cv2
-
-def initialize() -> tuple[CarlaSimulation, CarlaEgoVehicle]:
-    sim = CarlaSimulation(town_name="Town07")
-    return sim, sim.add_ego_vehicle(pos=[-90.0, 0, 2.0])
+from ultralytics import YOLO
 
 class ObjectPredictor(DiscreteComponent):
     def __init__(self, period_ms, camera: Camera):
@@ -14,16 +11,28 @@ class ObjectPredictor(DiscreteComponent):
         
     def _loop(self, dt: float) -> None:
         data, timestamp = self._camera.read()
+        
+        model = YOLO("yolov8n.pt")
+
+        # Run prediction on your PNG file
+        results = model(data)  # replace with your file path
+
+        # Save results to disk
+        results[0].save(filename="output.png")
+        
         if data is None:
             return
         cv2.imwrite("teste.png", data)
         pass
 
-    
+def initialize() -> tuple[CarlaSimulation, CarlaEgoVehicle]:
+    sim = CarlaSimulation(town_name="Town07")
+    return sim, sim.add_ego_vehicle(pos=[-90.0, 0, 2.0])    
 
 def main():
     sim, vehicle = initialize()
-    cam = vehicle.init_rgb_front_camera(width=512, height=512)
+    img_size = 1024
+    cam = vehicle.init_rgb_front_camera(width=img_size, height=img_size)
     
     predictor = ObjectPredictor(100, cam)
     predictor.start()
