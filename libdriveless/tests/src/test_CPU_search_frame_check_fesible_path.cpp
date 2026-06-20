0663cfdcfd9a748e584be737a6f32718d79c92f3
out0 <cristianoo@gmail.com>
2026-06-20 20:13:47 -0300

    adding test

--- libdriveless/tests/src/test_CPU_search_frame_check_fesible_path.cpp
+++ libdriveless/tests/src/test_CPU_search_frame_check_fesible_path.cpp
@@ -3,9 +3,6 @@
 #include "test_utils_cpu.h"
 #include <cmath>
 
-// CURVED
-// ---------------------------------------------------------------
-
 SearchFrameCPU *basicFrame()
 {
     SearchFrameCPU *f1 = new SearchFrameCPU(100, 100, {-1, -1}, {-1, -1}, 1);
@@ -40,6 +37,91 @@ TEST(TestSearchFrameCPUCheckFeasiblePath, CheckPath_Angle_NoPreProcess_CPU)
     frame->copyFrom(ptr);
 
     auto path1 = testInterpolateHermiteCurve(100, 100, Waypoint(50, 70, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0)));
+    //exportSearchFrameCPUToFile(*frame, "output1.png", path1);
+    ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, true));
+    ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, false));
+    for (auto p : path1)
+    {
+        if (!p.is_checked_as_feasible())
+            FAIL();
+    }
+
+    auto path2 = testInterpolateHermiteCurve(100, 100, Waypoint(32, 70, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0)));
+    //exportSearchFrameCPUToFile(*frame, "output2.png", path2);
+    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, true));
+    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, false));  
+}
+
+TEST(TestSearchFrameCPUCheckFeasiblePath, CheckPath_Angle_PreProcessNoVectorized_CPU)
+{
+    SearchFrameCPU *frame = basicFrame();
+
+    float *ptr = blankFrameData();
+    toFrameData(ptr, testInterpolateHermiteCurve(100, 100, Waypoint(30, 99, angle::deg(0.0)), Waypoint(30, 0, angle::deg(0.0))));
+    toFrameData(ptr, testInterpolateHermiteCurve(100, 100, Waypoint(70, 99, angle::deg(0.0)), Waypoint(70, 0, angle::deg(0.0))));
+
+    frame->copyFrom(ptr);
+    frame->processSafeDistanceZone({10, 10}, false);
+
+
+    auto path1 = testInterpolateHermiteCurve(100, 100, Waypoint(50, 70, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0)));
+    //exportSearchFrameCPUToFile(*frame, "output1.png", path1);
+    ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, true));
+    ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, false));
+    for (auto p : path1)
+    {
+        if (!p.is_checked_as_feasible())
+            FAIL();
+    }
+
+    auto path2 = testInterpolateHermiteCurve(100, 100, Waypoint(32, 70, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0)));
+    //exportSearchFrameCPUToFile(*frame, "output2.png", path2);
+    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, true));
+    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, false));
+
+    
+   
+}
+
+TEST(TestSearchFrameCPUCheckFeasiblePath, CheckPath_Angle_PreProcessWithVectorized_CPU)
+{
+    SearchFrameCPU *frame = basicFrame();
+
+    float *ptr = blankFrameData();
+    toFrameData(ptr, testInterpolateHermiteCurve(100, 100, Waypoint(30, 99, angle::deg(0.0)), Waypoint(30, 0, angle::deg(0.0))));
+    toFrameData(ptr, testInterpolateHermiteCurve(100, 100, Waypoint(70, 99, angle::deg(0.0)), Waypoint(70, 0, angle::deg(0.0))));
+
+    frame->copyFrom(ptr);
+    frame->processSafeDistanceZone({10, 10}, true);
+
+
+    auto path1 = testInterpolateHermiteCurve(100, 100, Waypoint(50, 70, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0)));
+    //exportSearchFrameCPUToFile(*frame, "output1.png", path1);
+    ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, true));
+    ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, false));
+    for (auto p : path1)
+    {
+        if (!p.is_checked_as_feasible())
+            FAIL();
+    }
+
+    auto path2 = testInterpolateHermiteCurve(100, 100, Waypoint(32, 70, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0)));
+    //exportSearchFrameCPUToFile(*frame, "output2.png", path2);
+    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, true));
+    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, false));
+}
+
+TEST(TestSearchFrameCPUCheckFeasiblePath, CheckPath_Curved)
+{
+    SearchFrameCPU *frame = basicFrame();
+
+    float *ptr = blankFrameData();
+    toFrameData(ptr, testInterpolateHermiteCurve(100, 100, Waypoint(30, 99, angle::deg(0.0)), Waypoint(10, 0, angle::deg(0.0))));
+    toFrameData(ptr, testInterpolateHermiteCurve(100, 100, Waypoint(70, 99, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0))));
+
+    frame->copyFrom(ptr);
+
+    auto path1 = testInterpolateHermiteCurve(100, 100, Waypoint(50, 70, angle::deg(0.0)), Waypoint(30, 0, angle::deg(0.0)));
     exportSearchFrameCPUToFile(*frame, "output1.png", path1);
     ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, true));
     ASSERT_TRUE(frame->checkFeasiblePath(path1, 10, 10, false));
@@ -52,113 +134,5 @@ TEST(TestSearchFrameCPUCheckFeasiblePath, CheckPath_Angle_NoPreProcess_CPU)
     auto path2 = testInterpolateHermiteCurve(100, 100, Waypoint(32, 70, angle::deg(0.0)), Waypoint(50, 0, angle::deg(0.0)));
     exportSearchFrameCPUToFile(*frame, "output2.png", path2);
     ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, true));
-    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, false));
-   
-}
-
-TEST(TestSearchFrameCPUCheckFeasiblePath, CheckPath_Angle_PreProcessNoVectorized_CPU)
-{
-    SearchFrameCPU f1(100, 100, {-1, -1}, {-1, -1});
-
-    std::vector<float> costs({{0.0},
-                              {-1.0}});
-    f1.setClassCosts(costs);
-
-    const int SIZE = 3 * 100 * 100;
-
-    float *ptr = new float[SIZE];
-    std::fill(ptr, ptr + SIZE, 0.0f);
-
-    auto c1 = testInterpolateHermiteCurve(100, 100, Waypoint(30, 99, angle::deg(0.0)), Waypoint(30, 0, angle::deg(0.0)));
-    for (auto p : c1)
-        ptr[3 * (p.z() * 100 + p.x())] = 1.0;
-    auto c2 = testInterpolateHermiteCurve(100, 100, Waypoint(70, 99, angle::deg(0.0)), Waypoint(70, 0, angle::deg(0.0)));
-    for (auto p : c2)
-        ptr[3 * (p.z() * 100 + p.x())] = 1.0;
-
-    f1.copyFrom(ptr);
-
-    f1.processSafeDistanceZone({10, 10}, false);
-
-    std::vector<Waypoint> path1;
-    for (int i = 99; i >= 0; i--)
-        path1.push_back(Waypoint(34, i, angle::rad(0)));
-
-    bool res = f1.checkFeasiblePath(path1, 10, 10, true);
-
-    std::vector<Waypoint> path2;
-    for (int i = 99; i >= 0; i--)
-        path2.push_back(Waypoint(50, i, angle::rad(0)));
-
-    bool res2 = f1.checkFeasiblePath(path2, 10, 10, true);
-
-    ASSERT_FALSE(res);
-
-    for (auto p : path1)
-    {
-        if (p.is_checked_as_feasible())
-            FAIL();
-    }
-
-    ASSERT_TRUE(res2);
-
-    for (auto p : path2)
-    {
-        if (!p.is_checked_as_feasible())
-            FAIL();
-    }
-}
-
-TEST(TestSearchFrameCPUCheckFeasiblePath, CheckPath_Angle_PreProcessWithVectorized_CPU)
-{
-    SearchFrameCPU f1(100, 100, {-1, -1}, {-1, -1});
-
-    std::vector<float> costs({{0.0},
-                              {-1.0}});
-    f1.setClassCosts(costs);
-
-    const int SIZE = 3 * 100 * 100;
-
-    float *ptr = new float[SIZE];
-    std::fill(ptr, ptr + SIZE, 0.0f);
-
-    auto c1 = testInterpolateHermiteCurve(100, 100, Waypoint(30, 99, angle::deg(0.0)), Waypoint(30, 0, angle::deg(0.0)));
-    for (auto p : c1)
-        ptr[3 * (p.z() * 100 + p.x())] = 1.0;
-    auto c2 = testInterpolateHermiteCurve(100, 100, Waypoint(70, 99, angle::deg(0.0)), Waypoint(70, 0, angle::deg(0.0)));
-    for (auto p : c2)
-        ptr[3 * (p.z() * 100 + p.x())] = 1.0;
-
-    f1.copyFrom(ptr);
-
-    // exportSearchFrameCPUToFile(f1, "output.png");
-    f1.processSafeDistanceZone({10, 10}, true);
-
-    std::vector<Waypoint> path1;
-    for (int i = 99; i >= 0; i--)
-        path1.push_back(Waypoint(34, i, angle::rad(0)));
-
-    bool res = f1.checkFeasiblePath(path1, 10, 10, true);
-
-    std::vector<Waypoint> path2;
-    for (int i = 99; i >= 0; i--)
-        path2.push_back(Waypoint(50, i, angle::rad(0)));
-
-    bool res2 = f1.checkFeasiblePath(path2, 10, 10, true);
-
-    ASSERT_FALSE(res);
-
-    for (auto p : path1)
-    {
-        if (p.is_checked_as_feasible())
-            FAIL();
-    }
-
-    ASSERT_TRUE(res2);
-
-    for (auto p : path2)
-    {
-        if (!p.is_checked_as_feasible())
-            FAIL();
-    }
+    ASSERT_FALSE(frame->checkFeasiblePath(path2, 10, 10, false));  
 }
